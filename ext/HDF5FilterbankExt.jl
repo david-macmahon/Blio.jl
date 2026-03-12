a8b158b6229415c9871681d00bca81ecf166accf
David MacMahon <davidm@astro.berkeley.edu>
2026-03-12 15:48:34 -0700

    Add copy support to fil2h5
    The `data` dataset created by `fil2h5` can now be an "external dataset"
    that references the data array that lives in the Filterbank file
    (exisiting functionality) as will as a copy of the data array that lives
    in the HDF5 file (new functionality).  When creating a copy of the data
    the user may specify chunk size and data filters (e.g. for data
    compression).

--- ext/HDF5FilterbankExt.jl
+++ ext/HDF5FilterbankExt.jl
@@ -1,37 +1,75 @@
 module HDF5FilterbankExt
 
-import Blio.Filterbank: Header, fil2h5
+import Blio.Filterbank: Header, fil2h5, mmap
 import Base.write
 import HDF5: write_attribute
 
 if isdefined(Base, :get_extension)
-    using HDF5: File, Dataset, h5open, attributes, create_dataset
+    using HDF5: File, Dataset, h5open, attributes, create_dataset, datatype,
+        write_dataset, Filters
 else
-    using ..HDF5: File, Dataset, h5open, attributes, create_dataset
+    using ..HDF5: File, Dataset, h5open, attributes, create_dataset, datatype,
+        write_dataset, Filters
 end
 
-function fil2h5(fbname, h5name="$fbname.h5"; kwargs...)
+"""
+    fil2h5(fbname[, h5name]; kwargs...)
+    fil2h5(fbname[, h5name]; copy=true, chunk, filters, kwargs...)
+
+Convert a SIGPROC filterbank file into an HDF5 file.
+
+The first form without the `copy` keyword argument (or with `copy=false`) will
+create an HDF5 file with an external `data` dataset (i.e. it will point to the
+data that lives inside the filterbank file).  The second form with `copy=true`
+will copy the data from the filterbank file into the HDF5 file.  In this case,
+the data will be chunked and compressed according the `chunk` and `filters`
+keyword arguments.
+
+In both forms, `h5name` will default to `fbname` with any `.fil` extension
+removed and a `.h5` extension added.  An exception will be thrown if `h5name`
+already exists.  Additional `kwargs` may be passed to provide additional (and/or
+corrected) attributes for the `data` dataset.
+
+The `chunk` keyword argument should be passed as a tuple of 3 integers.  The
+default value is `(512,1,16)`.
+
+The `filters` argument should be passed as an `HDF5.Filter` object or a Vector
+of `HDF5.Filter` objects.  The default for `filters` is `[Shuffle(),
+Deflate(3)]`, which uses filters that are built-in the HDF5 library.  Another
+option (of many) is to pass `filters=BitshuffleFilter(compressor=:lz4)` to use
+the bitshuffle filter from the `H5Zbitshuffle` package, which you would need to
+add as a dependency of your project because Blio does not depend on it.
+"""
+function fil2h5(fbname, h5name=replace(fbname, r"\.fil$"=>"") * ".h5";
+    copy=false, chunk=(512,1,16), filters=[Filters.Shuffle(), Filters.Deflate(3)],
+    kwargs...
+)
     ispath(h5name) && error("$h5name already exists")
 
     fbh = open(io->read(io, Header), fbname)
+
     h5 = h5open(h5name, "w")
-
     # For blimpy compatability
-    fattrs = attributes(h5)
-    fattrs["CLASS"] = "FILTERBANK"
-    fattrs["VERSION"] = "1.0"
+    write_attribute(h5, "CLASS", "FILTERBANK")
+    write_attribute(h5, "VERSION", "1.0")
 
+    # Create `data` dataset
     size = (fbh[:nchans], fbh[:nifs], fbh[:nsamps])
-    external = (fbname, fbh[:header_size], fbh[:data_size])
-    data = create_dataset(h5, "data", Float32, size; external)
-
-    attrs = attributes(data)
-    for k in keys(fbh)
-        attrs[string(k)] = fbh[k]
+    data = if copy
+        fbd = open(io->mmap(io, fbh), fbname)
+        ds = create_dataset(h5, "data", Float32, size; chunk, filters)
+        write_dataset(ds, datatype(fbd), fbd)
+        ds
+    else
+        external = (fbname, fbh[:header_size], fbh[:data_size])
+        create_dataset(h5, "data", Float32, size; external)
     end
+
+    # Write fbh as attributes of `data` datset
+    write_attribute(data, fbh)
     # Allow additional (or corrected) attributes from `kwargs`
-    for k in keys(kwargs)
-        attrs[string(k)] = kwargs[k]
+    for (k,v) in kwargs
+        write_attribute(data, string(k), v)
     end
 
     close(h5)
